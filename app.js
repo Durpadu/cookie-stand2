a79c311253a48ed15ba5c52406edc1aeac190f03
Durpadu <wells94e@gmail.com>
2019-02-16 15:06:44 -0800

    completed object literals for each store/pushing sales to array by time/appended to DOM

--- app.js
+++ app.js
@@ -1,68 +1,153 @@
-'use strict'
-
 'use strict';
+console.log('script loaded');
 
-var days = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-  'Sunday',
-];
+var storeTable= document.getElementById('stores');
 
-var alkiUl = document.getElementById('alkibeach');
-var goldenUl = document.getElementById('gg');
-var edmondsUl = document.getElementById('edmondsbeach');
+var storeTimes= [ '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm' ];
 
-var alki = {
-  name: 'Alki',
-  neighborhood: 'West Seattle',
-  seagullCount: [
-    212,
-    44,
-    34,
-    14,
-    15, 
-    23,
-    42,
-  ],
-  render: function () {
-    for(var i = 0; i < days.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = `${days[i]}: ${this.seagullCount[i]} seagulls.`;
-      alkiUl.appendChild(liEl);
-    }
-  }
-};
+var cookieTotal= [];
 
-var goldenGardens = {
-  name: 'Golden Gardens',
-  neighborhood: 'Crown Hill',
-  seagullCount: [2, 34, 67, 78, 98, 23, 101],
-  render: function () {
-    for(var i = 0; i < days.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = `${days[i]}: ${this.seagullCount[i]} seagulls.`;
-      goldenUl.appendChild(liEl);
-    }
-  }
-};
+var pikeUl = document.getElementById('pikeUl');
+var seatacUl = document.getElementById('setacUl');
+var seaCentUl = document.getElementById('seaCentUl');
+var capUl = document.getElementById('capUl');
+var alkiUl = document.getElementById('alkiUl');
 
-var edmonds = {
-  name: 'Edmonds',
-  neighborhood: 'North Seattle',
-  seagullCount: [24, 34, 54, 63, 75, 87, 94],
-  render: function () {
-    for(var i = 0; i < days.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = `${days[i]}: ${this.seagullCount[i]} seagulls.`;
-      edmondsUl.appendChild(liEl);
-    }
-  }
-};
+// function store(minCust, maxCust, aveCust) {
+//   this.minCust = minCust;
+//   this.maxCust = maxCust;
+//   this.aveCust = aveCust;
+//   this.cookieTotal= [];
+//   allStoreLocations.push(this);
+// }
+
+var pikeplace= {
+  minCust: 23,
+  maxCust: 65,
+  aveSale: 6.3,
+  newAveSale: 0,
+  timeCookieTotal: [],
+  randomCustomer: function () {
+    var min= Math.ceil(this.minCust);
+    var max= Math.floor(this.maxCust);
+    var multiplyRandCustAndAveSale= this.aveSale * Math.floor( Math.random() * (max-min) ) + min;
+    return Math.floor(multiplyRandCustAndAveSale);
+  },
+  render: function (){ 
+    for(var x = 0; x < storeTimes.length; x++) {
+    this.newAveSale= this.randomCustomer();
+    this.timeCookieTotal.push(this.newAveSale);
+    var liEl = document.createElement('li');
+    liEl.textContent = `${storeTimes[x]}: ${this.newAveSale}`
+    pikeUl.appendChild(liEl);
+    console.log(this.newAveSale)
+    };
+  },
+}
+
+pikeplace.render();
+
+
+var seatac= {
+  minCust: 3,
+  maxCust: 24,
+  aveSale: 1.2,
+  newAveSale: 0,
+  timeCookieTotal: [],
+  randomCustomer: function () {
+    var min= Math.ceil(this.minCust);
+    var max= Math.floor(this.maxCust);
+    var multiplyRandCustAndAveSale= this.aveSale * Math.floor( Math.random() * (max-min) ) + min;
+    return Math.floor(multiplyRandCustAndAveSale);
+  },
+  render: function (){ 
+    for(var x = 0; x < storeTimes.length; x++) {
+    this.newAveSale= this.randomCustomer();
+    this.timeCookieTotal.push(this.newAveSale);
+    var liEl = document.createElement('li');
+    liEl.textContent = `${storeTimes[x]}: ${this.newAveSale}`
+    setacUl.appendChild(liEl);
+    console.log(this.newAveSale)
+    };
+  },
+}
+
+seatac.render();
+
+var seattleCenter= {
+  minCust: 11,
+  maxCust: 28,
+  aveSale: 3.7,
+  newAveSale: 0,
+  timeCookieTotal: [],
+  randomCustomer: function () {
+    var min= Math.ceil(this.minCust);
+    var max= Math.floor(this.maxCust);
+    var multiplyRandCustAndAveSale= this.aveSale * Math.floor( Math.random() * (max-min) ) + min;
+    return Math.floor(multiplyRandCustAndAveSale);
+  },
+  render: function (){ 
+    for(var x = 0; x < storeTimes.length; x++) {
+    this.newAveSale= this.randomCustomer();
+    this.timeCookieTotal.push(this.newAveSale);
+    var liEl = document.createElement('li');
+    liEl.textContent = `${storeTimes[x]}: ${this.newAveSale}`
+    seaCentUl.appendChild(liEl);
+    console.log(this.newAveSale)
+    };
+  },
+}
+
+seattleCenter.render();
+
+var capHill= {
+  minCust: 20,
+  maxCust: 38,
+  aveSale: 2.3,
+  newAveSale: 0,
+  timeCookieTotal: [],
+  randomCustomer: function () {
+    var min= Math.ceil(this.minCust);
+    var max= Math.floor(this.maxCust);
+    var multiplyRandCustAndAveSale= this.aveSale * Math.floor( Math.random() * (max-min) ) + min;
+    return Math.floor(multiplyRandCustAndAveSale);
+  },
+  render: function (){ 
+    for(var x = 0; x < storeTimes.length; x++) {
+    this.newAveSale= this.randomCustomer();
+    this.timeCookieTotal.push(this.newAveSale);
+    var liEl = document.createElement('li');
+    liEl.textContent = `${storeTimes[x]}: ${this.newAveSale}`
+    capUl.appendChild(liEl);
+    console.log(this.newAveSale)
+    };
+  },
+}
+
+capHill.render();
+
+var alki= {
+  minCust: 2,
+  maxCust: 16,
+  aveSale: 4.6,
+  newAveSale: 0,
+  timeCookieTotal: [],
+  randomCustomer: function () {
+    var min= Math.ceil(this.minCust);
+    var max= Math.floor(this.maxCust);
+    var multiplyRandCustAndAveSale= this.aveSale * Math.floor( Math.random() * (max-min) ) + min;
+    return Math.floor(multiplyRandCustAndAveSale);
+  },
+  render: function (){ 
+    for(var x = 0; x < storeTimes.length; x++) {
+    this.newAveSale= this.randomCustomer();
+    this.timeCookieTotal.push(this.newAveSale);
+    var liEl = document.createElement('li');
+    liEl.textContent = `${storeTimes[x]}: ${this.newAveSale}`
+    alkiUl.appendChild(liEl);
+    console.log(this.newAveSale)
+    };
+  },
+}
 
 alki.render();
-goldenGardens.render();
-edmonds.render();
